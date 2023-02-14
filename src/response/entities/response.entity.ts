1ab2c417a40d665a3a62a858a4140c610bc9ebc4
Jungsoyi <soi0125@naver.com>
2023-02-14 15:28:42 +0900

    :sparkles: creat response entity

--- src/response/entities/response.entity.ts
+++ src/response/entities/response.entity.ts
@@ -1,7 +1,42 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, GraphQLTimestamp } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Response {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  responseId: number
+
+  @Field(() => String)
+  @Column()
+  surveyTitle: string;
+
+  @Field(() => Int)
+  @Column()
+  questionNumber: number;
+
+  @Field(() => String)
+  @Column()
+  questionContent: string;
+
+  @Field(() => Int)
+  @Column()
+  answerNumber: number;
+
+  @Field(() => String)
+  @Column()
+  answerContent: string;
+
+  @Field(() => Int)
+  @Column()
+  answerScore: number;
+
+  @Field(() => GraphQLTimestamp)
+  @Column()
+  createdAt: Date;
+
+  @Field(() => GraphQLTimestamp)
+  @Column()
+  modifiedAt: Date;
 }
