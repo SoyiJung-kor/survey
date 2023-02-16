0943c97ce41a7ce92674ff4e9290b415e9fe4a60
Jungsoyi <soi0125@naver.com>
2023-02-16 13:54:30 +0900

    :sparkles: create response crud

--- src/response/dto/create-response.input.ts
+++ src/response/dto/create-response.input.ts
@@ -2,9 +2,6 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateResponseInput {
-  @Field(() => Int, { description: 'participant Id' })
+  @Field(() => Int)
   participantId: number;
-
-  @Field(() => Int, { description: 'survey Id' })
-  surveyId: number;
 }
