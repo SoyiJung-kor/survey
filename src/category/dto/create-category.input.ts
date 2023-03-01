f34c88da5bd95511bae79ab5e4fff484094d39fd
JungSoyi <soi0125@naver.com>
2023-03-01 15:10:25 +0900

    :hammer: dto 수정

--- src/category/dto/create-category.input.ts
+++ src/category/dto/create-category.input.ts
@@ -1,7 +1,14 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNumber, IsString, MinLength } from 'class-validator';
 
 @InputType()
 export class CreateCategoryInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  @IsString()
+  @MinLength(2)
+  categoryName: string;
+
+  @Field(() => Int)
+  @IsNumber()
+  surveyId: number;
 }
