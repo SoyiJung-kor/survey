8ba74325e21411eb09516aef7840c04f8607cf05
JungSoyi <soi0125@naver.com>
2023-03-01 15:08:16 +0900

    :card_file_box: category 연관관계 설정

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,7 +1,21 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
+import { Column, JoinColumn, ManyToOne } from 'typeorm';
+import { CommonEntity } from '../../common/entity/\bcommon.entity';
+import { Survey } from '../../survey/entities/survey.entity';
 
 @ObjectType()
-export class Category {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class Category extends CommonEntity {
+  @Field(() => String)
+  @IsString()
+  categoryName: string;
+
+  @Column()
+  surveyId: number;
+
+  @ManyToOne(() => Survey, (survey) => survey.categories, { onDelete: 'CASCADE', })
+  @Field(() => Survey)
+  @JoinColumn({ name: 'surveyId' })
+  survey: Survey;
+
 }
