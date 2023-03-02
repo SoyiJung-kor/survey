87e628cc7ed00cd505a8b4f207171a1fbf514f1b
SoyiJung-kor <soyi@maumlab.co.kr>
2023-03-02 14:19:47 +0900

    :hammer: dto update

--- src/category-score/dto/create-category-score.input.ts
+++ src/category-score/dto/create-category-score.input.ts
@@ -1,7 +1,24 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNumber } from 'class-validator';
 
 @InputType()
 export class CreateCategoryScoreInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  @IsNumber()
+  gradeScore: number;
+
+  @Field(() => String)
+  @IsString()
+  categoryMessage: string;
+
+  @Field()
+  @IsNumber()
+  categoryId: number;
+
+  @Field(() => Category)
+  @JoinColumn({ name: 'categoryId' })
+  @ManyToOne(() => Category, (category) => category.categoryScores, {
+    onDelete: 'CASCADE',
+  })
+  category: Category;
 }
--- src/category-score/dto/update-category-score.input.ts
+++ src/category-score/dto/update-category-score.input.ts
@@ -2,7 +2,9 @@ import { CreateCategoryScoreInput } from './create-category-score.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateCategoryScoreInput extends PartialType(CreateCategoryScoreInput) {
+export class UpdateCategoryScoreInput extends PartialType(
+  CreateCategoryScoreInput,
+) {
   @Field(() => Int)
   id: number;
 }
