5c56079863486a7d7991bb5722682a526392261c
SoyiJung-kor <soyi@maumlab.co.kr>
2023-03-02 14:17:43 +0900

    :card_file_box: category categoryscore 연관관계 만들기

--- src/category-score/entities/category-score.entity.ts
+++ src/category-score/entities/category-score.entity.ts
@@ -1,7 +1,29 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { IsNumber, IsString } from 'class-validator';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Category } from '../../category/entities/category.entity';
+import { CommonEntity } from '../../common/entities/\bcommon.entity';
 
 @ObjectType()
-export class CategoryScore {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+@Entity()
+export class CategoryScore extends CommonEntity {
+  @Field(() => Int)
+  @Column()
+  @IsNumber()
+  gradeScore: number;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  categoryMessage: string;
+
+  @Column()
+  categoryId: number;
+
+  @Field(() => Category)
+  @JoinColumn({ name: 'categoryId' })
+  @ManyToOne(() => Category, (category) => category.categoryScores, {
+    onDelete: 'CASCADE',
+  })
+  category: Category;
 }
