37f5ee4099c3f1acfd3bb960d19d346c6e3a370b
SoyiJung-kor <soyi@maumlab.co.kr>
2023-03-07 11:38:53 +0900

    :card_file_box: autoloadentities 설정 추가

--- src/common/config/test-orm-config.ts
+++ src/common/config/test-orm-config.ts
@@ -1,15 +1,5 @@
 /* eslint-disable prettier/prettier */
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Answer } from '../../answer/entities/answer.entity';
-import { CategoryScore } from '../../category-score/entities/category-score.entity';
-import { Category } from '../../category/entities/category.entity';
-import { EachResponse } from '../../each-response/entities/each-response.entity';
-import { Participant } from '../../participant/entities/participant.entity';
-import { QuestionCategory } from '../../question-category/entities/question-category.entity';
-import { Question } from '../../question/entities/question.entity';
-import { ResponseCategory } from '../../response-category/entities/response-category.entity';
-import { Response } from '../../response/entities/response.entity';
-import { Survey } from '../../survey/entities/survey.entity';
 
 export const testTypeORMConfig: TypeOrmModuleOptions = {
     type: 'postgres',
@@ -18,6 +8,6 @@ export const testTypeORMConfig: TypeOrmModuleOptions = {
     username: 'postgres',
     password: 'postgres',
     database: 'test',
-    entities: [Answer, Participant, Question, Response, Survey, EachResponse, Category, CategoryScore, QuestionCategory, ResponseCategory],
+    autoLoadEntities: true,
     synchronize: true,
 };
