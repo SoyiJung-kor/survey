4d3d7e9436f931c813f84bc9e4f9ab4328c30c93
SoyiJung-kor <soyi@maumlab.co.kr>
2023-03-10 10:16:51 +0900

    :recycle: survey repository 생성

---
+++ src/survey/repositories/survey.repository.ts
@@ -0,0 +1,28 @@
+import { Repository } from "typeorm";
+import { Survey } from "../entities/survey.entity";
+
+export interface SurveyRepository extends Repository<Survey> {
+    this: Repository<Survey>
+    findSurveyWithQuestion(questionId: number): Promise<Survey>;
+    findSurveyWithCategory(categoryName: string): Promise<Survey[]>;
+}
+
+type CustomSurveyRepository = Pick<SurveyRepository, 'findSurveyWithQuestion' | 'findSurveyWithCategory'>;
+
+export const CustomSurveyRepositoryMethods: CustomSurveyRepository = {
+    async findSurveyWithQuestion(questionId: number) {
+        return await this.surveys
+            .createQueryBuilder()
+            .innerJoinAndSelect(`survey`, `question.surveyId`)
+            .where(`question.id = ${questionId}`)
+            .where(`survey.id = question.surveyId`)
+            .getOne();
+    },
+
+    async findSurveyWithCategory(categoryName: string) {
+        return this.surveyRepository.createQueryBuilder()
+            .innerJoinAndSelect(`survey.categories`, `category`)
+            .where(`category.catogoryName = ${categoryName}`)
+            .getMany();
+    }
+}
